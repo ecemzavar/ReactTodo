3a30f94574712ed78d9de6d123201092c55e0705
ecemzavar <88984118+ecemzavar@users.noreply.github.com>
2021-09-20 21:29:10 +0300

    ReactTodo

---
+++ reacttodo/begin-react/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState} from 'react'
+import { useTodoLayerValue } from './context/TodoContext';
+import TodoList from './components/TodoList';
+import './App.css';
+
+const App = () => {
+  const [ { todos} , dispatch] = useTodoLayerValue();
+  const [ content , setContent] =  useState('');
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!content &&  content.length <1 ) return;
+
+    const newTodo ={
+      id: Math.floor(Math.random() *  428374324),
+      content,
+      isCompleted: false,
+    };
+
+    dispatch({
+      type: 'ADD_TODO' ,
+      payload: newTodo,
+    });
+
+    setContent(' ');
+  };
+
+  
+  
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="todo-form">
+        <input
+        type="text"
+        className="todo-input"
+        onChange={(event) => setContent(event.target.value)}
+        value={content}
+        
+        />
+        <button className="todo-button">Ekle</button>
+      </form>
+      {/* Todo Listesi */}
+      <TodoList todos={todos} />
+    </div>
+  );
+};
+
+export default App;
